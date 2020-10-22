77984c3c9547bbfb9ee807900f002e15ceff58d8
UDannyf <danny_u89@hotmail.com>
2020-10-22 16:21:10 -0500

    Insurance function and validate information function

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -2,24 +2,49 @@ package ec.edu.espol.workshops.second;
 
 public class CarInsurance {
 	private int ageClient;
-	private char[] sexClient = {'M','F'};
-	private String[] maritalStatus= {"married","notMarried"};
-	private boolean licenseCliente;
+	private char genClient;
+	private char marriedStatus;
+	private char licenseCliente;
 	private final int BASE = 500;
-	
 	public int getAgeClient() {
 		return ageClient;
 	}
 	public void setAgeClient(int ageClient) {
 		this.ageClient = ageClient;
 	}
+	public char getGenClient() {
+		return genClient;
+	}
+	public void setGenClient(char genClient) {
+		this.genClient = genClient;
+	}
 	
-	public char[] getSexClient() {
-		return sexClient;
+	
+	//return the value of the insurance charge or -1 if not applicable
+	public int calcularPrima() {
+		if(this.getAgeClient()<25 && this.getGenClient()=='M' && this.getMarriedStatus()=='N') {
+			return BASE+1500;
+		}
+		else if(this.getGenClient()=='F' || this.getMarriedStatus()=='Y') {
+			return BASE-200;
+		}
+		else if(this.getAgeClient()>45 && this.getAgeClient()<65) {
+			return BASE-100;
+		}
+		return -1;
 	}
-	public void setSexClient(char[] sexClient) {
-		this.sexClient = sexClient;
+	
+	//return true or false if customer information does not meet the requirements
+	public boolean validarPolitica() {
+		if(this.getAgeClient()>80) {
+			return false;
+		}
+		else if(this.getLicenseCliente()=='Y') {
+			return true;
+		}
+		return false;
 	}
+
 	
 	
 
