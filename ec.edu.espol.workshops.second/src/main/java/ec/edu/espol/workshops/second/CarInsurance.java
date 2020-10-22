75527ebffaf5de590e3c7e7fee9bd43fa8439c7c
UDannyf <danny_u89@hotmail.com>
2020-10-22 13:47:56 -0500

    Edad, Sexo  (Get - Set)

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -2,9 +2,25 @@ package ec.edu.espol.workshops.second;
 
 public class CarInsurance {
 	private int ageClient;
-	private char genClient;
-	private char marriedStatus;
-	private char notMarriedStatus;
+	private char[] sexClient = {'M','F'};
+	private String[] maritalStatus= {"married","notMarried"};
 	private boolean licenseCliente;
 	private final int BASE = 500;
+	
+	public int getAgeClient() {
+		return ageClient;
+	}
+	public void setAgeClient(int ageClient) {
+		this.ageClient = ageClient;
+	}
+	
+	public char[] getSexClient() {
+		return sexClient;
+	}
+	public void setSexClient(char[] sexClient) {
+		this.sexClient = sexClient;
+	}
+	
+	
+
 }
