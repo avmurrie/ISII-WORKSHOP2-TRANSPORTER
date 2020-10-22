246e4f1eefa90a53f843a5e75363f47acab1607c
Edison Rosales <edisonrosales94@gmail.com>
2020-10-22 13:35:50 -0500

    Declare constant and attributes

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,5 +1,10 @@
 package ec.edu.espol.workshops.second;
 
 public class CarInsurance {
+	private int ageClient;
+	private char[] sexClient = {'M','F'};
+	private String[] maritalStatus= {"married","notMarried"};
+	private boolean licenseCliente;
+	private final int BASE = 500;
 
 }
