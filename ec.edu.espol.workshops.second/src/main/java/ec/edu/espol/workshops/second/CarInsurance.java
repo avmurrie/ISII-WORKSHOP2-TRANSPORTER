6d50ad0946b6036304786f6f057046987ead9b67
avmurrie <avmurrie@espol.edu.ec>
2020-10-30 02:32:36 -0500

    checkstytle

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,23 +1,23 @@
 package ec.edu.espol.workshops.second;
 
 public class CarInsurance {
-	private int ageClient;
-	private char genClient;
-	private char marriedStatus;
-	private char licenseClient;
-	private final int BASE = 500;
+  private int ageClient;
+  private char genClient;
+  private char marriedStatus;
+  private char licenseClient;
+  private static final int base = 500; // NOPMD by User on 30/10/20 00:13
+
+  public CarInsurance(int age, char gen, char marriedStatus, char licenseClient) {
+    this.ageClient = age;
+    this.genClient = gen;
+    this.marriedStatus = marriedStatus;
+    this.licenseClient = licenseClient;
+  }
 	
-	public CarInsurance(int age, char gen, char marriedStatus,char licenseClient) {
-		 this.ageClient = age;
-		 this.genClient = gen;
-		 this.marriedStatus = marriedStatus;
-		 this.licenseClient = licenseClient;
-	}
 	
-	
-	public int getAgeClient() {
-		return ageClient;
-	}
+  public int getAgeClient() {
+    return ageClient;
+  }
 	public void setAgeClient(int ageClient) {
 		this.ageClient = ageClient;
 	}
@@ -41,25 +41,25 @@ public class CarInsurance {
 		this.licenseClient = licenseClient;
 	}
 	//return the value of the insurance charge or -1 if not applicable
-	public int calcularPrima() {
-		if(this.getAgeClient()<25 && this.getGenClient()=='M' && this.getMarriedStatus()=='N') {
-			return BASE+1500;
-		}
-		else if(this.getGenClient()=='F' || this.getMarriedStatus()=='Y') {
-			return BASE-200;
-		}
-		else if(this.getAgeClient()>45 && this.getAgeClient()<65) {
-			return BASE-100;
-		}
-		return -1;
-	}
+	
+  public int calcularPrima() {
+    if (this.getAgeClient() < 25 && this.getGenClient() == 'M' 
+        && this.getMarriedStatus() == 'N') {
+      return base + 1500;
+    } else if (this.getGenClient() == 'F' || this.getMarriedStatus() == 'Y') {
+      return base - 200;
+    } else if (this.getAgeClient() > 45 && this.getAgeClient() < 65) {
+      return base - 100;
+    }
+    return -1;
+  }
 	
 	//return true or false if customer information does not meet the requirements
 	public boolean validarPolitica() {
-		if(this.getAgeClient()>80) {
+		if(this.getAgeClient() > 80) {
 			return false;
 		}
-		else if(this.getLicenseClient()=='Y') {
+		else if(this.getLicenseClient() == 'Y') {
 			return true;
 		}
 		return false;
