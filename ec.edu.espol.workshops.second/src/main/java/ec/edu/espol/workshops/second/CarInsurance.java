8c4b375bf7aad88bc9483c3e8533dc4238a05bf6
UDannyf <danny_u89@hotmail.com>
2020-11-08 13:26:50 -0500

    corrections test plan

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,11 +1,13 @@
 package ec.edu.espol.workshops.second;
 
+import java.lang.*;
+
 public class CarInsurance {
 	private int ageClient;
 	private char genClient;
 	private char marriedStatus;
 	private char licenseClient;
-	private final int BASE = 500;
+	protected static final int BASE = 500;
 	
 	public CarInsurance(int age, char gen, char marriedStatus,char licenseClient) {
 		 this.ageClient = age;
@@ -42,26 +44,26 @@ public class CarInsurance {
 	}
 	//return the value of the insurance charge or -1 if not applicable
 	public int calcularPrima() {
-		if(this.getAgeClient()<25 && this.getGenClient()=='M' && this.getMarriedStatus()=='N') {
+		if(this.getAgeClient()<25 && Character.toUpperCase(this.getGenClient()) =='M' && Character.toUpperCase(this.getMarriedStatus())=='N') {
 			return BASE+1500;
 		}
-		else if(this.getGenClient()=='F' || this.getMarriedStatus()=='Y') {
+		else if(Character.toUpperCase(this.getGenClient())=='F' || Character.toUpperCase(this.getMarriedStatus())=='Y') {
 			return BASE-200;
 		}
 		else if(this.getAgeClient()>45 && this.getAgeClient()<65) {
 			return BASE-100;
 		}
-		return -1;
+		return BASE;
 	}
 	
 	//return true or false if customer information does not meet the requirements
 	public boolean validarPolitica() {
-		if(this.getAgeClient()>80) {
+		if(this.getAgeClient()>80 || this.getAgeClient() < 0) {
 			return false;
 		}
-		else if(this.getLicenseClient()=='Y') {
+		else if(Character.toUpperCase(this.getLicenseClient())=='Y') {			
 			return true;
-		}
+		}		
 		return false;
 	}
 
--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Main.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Main.java
@@ -6,29 +6,31 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args)  {
 		CarInsurance usuario;
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in,"UTF-8");       
         System.out.println("Welcome");
         try {
-        System.out.println("Age of the customer");
-        int edad=sc.nextInt();
-        System.out.println("Gender of the customer (F Femae / M Male) ");
-        char gen=sc.next().charAt(0);
-        System.out.println("Marital status of the customer (Y Married / N Not married)");
-        char estado=sc.next().charAt(0);
-        System.out.println("Have the customer valid driving license? (Y Yes / N No)");
-        char licencia=sc.next().charAt(0);
-
-        usuario= new CarInsurance(edad,gen,estado,licencia);
-        if(usuario.validarPolitica()) {
-        	System.out.println("The value of the premium is:"+	usuario.calcularPrima());
+        	System.out.println("Age of the customer");
+        	int edad=sc.nextInt();
+        	System.out.println("Gender of the customer (F Femae / M Male) ");
+        	char gen=sc.next().charAt(0);
+        	System.out.println("Marital status of the customer (Y Married / N Not married)");
+        	char estado=sc.next().charAt(0);
+        	System.out.println("Have the customer valid driving license? (Y Yes / N No)");
+        	char licencia=sc.next().charAt(0);
+        	usuario= new CarInsurance(edad,gen,estado,licencia);
+        	if(usuario.validarPolitica()) {
+        		System.out.println("The value of the premium is:"+	usuario.calcularPrima());
+        	}
+        	else {
+        		System.out.println("The customer does not apply to a valid premium");
+        	}
         }
-        else {
-        	System.out.println("The customer does not apply to a valid premium");
+        catch(Exception e) {
+        	System.out.println("Error in input values");
         }
-        }
-        catch(Exception e){
-        	System.out.println("Invalid data");
-        }
+        finally {
+	        sc.close();
+	    }
         
 	}
 }
